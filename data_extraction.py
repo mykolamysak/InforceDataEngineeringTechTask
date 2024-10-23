be7c976036a8841555172c2094f17eea2872f4cb
Mykola Mysak <112685626+mykolamysak@users.noreply.github.com>
2024-10-23 18:43:29 +0300

    Initial commit

---
+++ data_extraction.py
@@ -0,0 +1,33 @@
+import csv
+import faker  # generates names, emails, etc
+import pandas as pd
+import random
+
+fake = faker.Faker()
+
+NUM_RECORDS = 1001
+FILENAME = 'user_data.csv'
+
+# Email domains
+custom_domains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com', 'ukr.net', 'mailbox.org', 'example.io']
+
+# Generate CSV
+with open(FILENAME, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['user_id', 'name', 'email', 'signup_date'])
+
+    for user_id in range(1, NUM_RECORDS + 1):
+        name = fake.name()
+
+        # Email generation
+        local_part = fake.user_name()
+        domain = random.choice(custom_domains)
+        email = f"{local_part}@{domain}"
+
+        signup_date = fake.date_time_this_decade().strftime('%Y-%m-%d %H:%M:%S')
+
+        writer.writerow([user_id, name, email, signup_date])
+
+# Read from CSV
+df = pd.read_csv(FILENAME)
+print(df.head())
---
+++ main.py
@@ -0,0 +1,45 @@
+import re
+import pandas as pd
+
+
+def read_csv_file(file_path):
+    """Reads the .csv file"""
+
+    return pd.read_csv(file_path)
+
+
+def convert_signup_date_format(data_frame):
+    """Converts time format using Pandas"""
+
+    data_frame['signup_date'] = pd.to_datetime(data_frame['signup_date']).dt.strftime('%Y-%m-%d')
+    return data_frame
+
+
+def filter_invalid_emails(data_frame):
+    """Filter rows where the 'email' field matches the valid email pattern"""
+
+    # Email basic validation (source: https://uibakery.io/regex-library/email-regex-python)
+    email_pattern = r"^\S+@\S+\.\S+$"
+
+    data_frame = data_frame[data_frame['email'].apply(lambda x: re.match(email_pattern, x) is not None)]
+    return data_frame
+
+
+def add_email_domain_column(data_frame):
+    """Extracts the domain from email field and adds it as a column 'domain'"""
+
+    data_frame['domain'] = data_frame['email'].apply(lambda x: x.split('@')[1])
+    return data_frame
+
+
+if __name__ == "__main__":
+    FILE_PATH = 'user_data.csv'
+
+    data_frame = read_csv_file(FILE_PATH)
+    data_frame = convert_signup_date_format(data_frame)
+    data_frame = filter_invalid_emails(data_frame)
+    data_frame = add_email_domain_column(data_frame)
+    print(data_frame.head())
+
+    # Save into a new file
+    data_frame.to_csv('updated_user_data.csv', index=False)
